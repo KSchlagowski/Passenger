b0f47e63dca7f8919e802358dc3d7064e4eb6d28
KSchlagowski <mediewilnp@gmail.com>
2020-08-17 21:46:06 +0200

    Resolve yeoman incompability

--- Passenger.Core/obj/Debug/netstandard1.4/Passenger.Core.AssemblyInfo.cs
+++ Passenger.Core/obj/Debug/netstandard1.4/Passenger.Core.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Passenger.Api")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Passenger.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Passenger.Api")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Passenger.Api")]
+[assembly: System.Reflection.AssemblyProductAttribute("Passenger.Core")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Passenger.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Wygenerowane przez klasę WriteCodeFragment programu MSBuild.
